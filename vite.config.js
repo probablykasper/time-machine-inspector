0ec748d8ff39c8e6aa19b04e599998a779aa9e4e
Kasper <kasperkh.kh@gmail.com>
2022-01-03 04:32:50 +0100

    Add autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   root: './src',
@@ -17,6 +18,11 @@ export default defineConfig({
     minify: false,
     sourcemap: true,
     target: ['chrome64', 'edge79', 'firefox62', 'safari11.1'],
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
   },
   plugins: [
     svelte({
