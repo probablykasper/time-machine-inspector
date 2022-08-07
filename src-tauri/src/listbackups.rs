b4e70d699c68ef9e0a8b522d97252c663c42561d
Kasper <kasperkh.kh@gmail.com>
2022-08-07 19:10:04 +0200

    Fix "No parent of path" (#5)

--- src-tauri/src/listbackups.rs
+++ src-tauri/src/listbackups.rs
@@ -3,12 +3,6 @@ use crate::dir_map::DirMap;
 use std::process::Command;
 
 pub fn listbackups() -> Result<DirMap<()>, String> {
-  // return Ok(Some(
-  //   "/Volumes/Time Machine Backups/Backups.backupdb/my-mac/2021-12-21-133750\n\
-  //   /Volumes/Time Machine Backups/Backups.backupdb/my-mac/2021-12-27-193733\n"
-  //     .to_string(),
-  // ));
-
   println!("tmutil listbackups");
 
   let output = Command::new("tmutil")
@@ -26,12 +20,9 @@ pub fn listbackups() -> Result<DirMap<()>, String> {
     .filter(|s| s != "")
     .collect();
 
-  let mut machine_dir = None;
-  for path in &mut paths {
-    if path.starts_with("/") {
-      continue;
-    }
-    match &machine_dir {
+  let mut stored_machine_dir = None;
+  let mut get_machine_dir = || -> Result<String, String> {
+    match &stored_machine_dir {
       None => {
         println!("tmutil machinedirectory");
         let output = Command::new("tmutil")
@@ -40,12 +31,19 @@ pub fn listbackups() -> Result<DirMap<()>, String> {
           .expect("Error calling command");
         check_cmd_success(&output.status, output.stderr.clone())?;
         println!("Success getting machinedirectory");
-        let output_str = parse_output(output.stdout)?;
-        machine_dir = Some(output_str);
+        let output_str = parse_output(output.stdout)?.trim().to_string();
+        stored_machine_dir = Some(output_str.clone());
+        Ok(output_str)
       }
-      Some(machine_dir) => {
-        *path = machine_dir.to_string() + "/" + &path;
-      }
+      Some(machine_dir) => Ok(machine_dir.clone()),
+    }
+  };
+  for path in &mut paths {
+    if path.starts_with("/") {
+      continue;
+    } else {
+      let machine_dir = get_machine_dir()?;
+      *path = machine_dir + "/" + &path;
     }
   }
   if let Some(path0) = paths.get(0) {
